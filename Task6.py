14aa59fa9eb6c768408234a5c246523722b676fe
avishekaryan <ai.avhi.aryan@gmail.com>
2026-02-22 23:00:22 +0545

    Marksheet

--- Task6.py
+++ Task6.py
@@ -13,6 +13,40 @@ Find percentage and display the grade. """
 
 
 # Solution
+name = input("Enter the name of student: ")
+a = float(input("Enter marks obtained in AI : "))
+p = float(input("Enter marks obtained in Python: "))
+s = float(input("Enter marks obtained in Sociology: "))
+c = float(input("Enter marks obtained in Calculus: "))
+e = float(input("Enter marks obtained in English: "))
+d = float(input("Enter marks obtained in Digital logic: "))
 
+total_obtained = a + p + s + c + e + d
+print(f"Total marks of {name} is: ", total_obtained)
+
+if a>=32 and p>=32 and s>=32 and c>=32 and e>=32 and d>=32 and total_obtained>=240 :
+    print("**Pass**")
+else :
+    print("**Fail**")
+
+percentage = ( total_obtained / 600 )* 100
+print("Percentage = " , percentage ,"%")
+
+if percentage > 90 :
+    print("Grade : **A+**")
+elif percentage >80 :
+    print("Grade : **A**")
+elif percentage >70 :
+    print("Grade : **B+**")
+elif percentage >60 :
+    print("Grade : **B**")
+elif percentage >50 :
+    print("Grade : **C+**")
+elif percentage >40 :
+    print("Grade : **C**")
+elif percentage >35 :
+    print("Grade : **D**")
+else :
+    print("Grade : **NG**")
 
 
