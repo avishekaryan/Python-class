8534cf4aca13106fe366a64f0769d3f2c8806b5c
avishekaryan <ai.avhi.aryan@gmail.com>
2026-02-23 17:48:47 +0545

    List syntax , in-built functions and methods with example

--- list.py
+++ list.py
@@ -13,10 +13,10 @@ print(List_data[4])     # [0,1,2,3,4,5,6,7] 4th position ko value print garcha
 print(List_data[-1])    # last value print garcha 
 
 # list slicing
-print(List_data[1 : 4])   # (start : stop : step)
+print(List_data[1 : 4])   # (start : stop : step)  yesma 1 ra 4 ni index position ko value ho 
 
 # reverse
-print(List_data[ ::-1])
+print(List_data[ ::-1])     # list ko value lai reverse order ma print garcha
 
 
 # Python In-built fuction for list
@@ -44,26 +44,29 @@ print(a)
 
 # list methods
 
-list_num.append(16)
+list_num.append(16)        # list ko last ma value add garcha
 print(list_num)
 
-list_num.insert(5,3)   #insert(index position , value)
+list_num.insert(5,3)       #insert(index position , value)
 print(list_num)
 
 
-list_num.remove(3)
+list_num.remove(3)          # list bata value lai remove garcha, by default first index position ko value lai remove garcha 
 print(list_num)
 
-list_num.pop(3)
+list_num.pop(3)             # index position ko value lai remove garcha, by default last index position ko value lai remove garcha
 print(list_num)
 
-list_num.sort()
+list_num.sort()             # list ko values lai ascending order ma rakhcha
 print(list_num)
 
-list_num.extend([12,34,56])
+list_num.extend([12,34,56]) #insert multiple values at once
 print(list_num)
 
-list_num.clear()
+list_num.reverse()          # list ko values lai reverse order ma rakhcha
+print(list_num)
+
+list_num.clear()         # puraie list lai empty garcha
 print(list_num)
 
 list_num.append(6)
