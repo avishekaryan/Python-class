efe6595bff09ee8248fbf60b3c423266d2fb09df
avishekaryan <ai.avhi.aryan@gmail.com>
2026-02-23 17:57:16 +0545

    input name , roll no , faculty and marks using eval

--- Task2.py
+++ Task2.py
@@ -1,14 +1,19 @@
 # Question
-"""Take a input from user
+"""
+Take a input from user
 string(name)
 int(roll no)
 expression(marks) - eval()
-and print total marks in float value """
+and print total marks in float value 
+
+"""
+
 
 # Answer
 
 name=input("Enter your name: ")
 Roll_no=int(input("Enter your roll no: "))
-marks=input("Enter your obtained marks of each subject: ")
+faculy = input("Enter your faculty : ")
+marks=input("Enter your obtained marks of each subject: ")  # eg. 80+90+85
 Total_marks=eval(marks)
 print("Total_marks : ",Total_marks)
