6e8ba24a05347f28f2f93ccd89920c29b8d1f3bb
avishekaryan <ai.avhi.aryan@gmail.com>
2026-02-23 17:06:28 +0545

    Eligible to vote or not

--- vote.py
+++ vote.py
@@ -1,15 +1,18 @@
 # WAP to find if a person is eligiable to vote or not.
 
 age = int(input("Enter your age: "))
+
 if age >= 18 :
     print("You are eligible to vote")
 else:
     print("Not eligible to vote")
 
 
+
 # WAP to find if a person is eligiable to vote or not in Nepal.
 
 age = int(input("Enter your age: "))
+
 if age >= 18 :
     citizen = input("Enter the country name : ")
     if citizen == "Nepal" or citizen =="nepal" :
