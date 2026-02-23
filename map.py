eb7be686d567c6a36795bf4f1de8b52e82bb23db
avishekaryan <ai.avhi.aryan@gmail.com>
2026-02-23 17:26:47 +0545

    Taking multiple inputs in a single line and thier arthematic operations and average

--- map.py
+++ map.py
@@ -2,8 +2,22 @@
 
 x,y,z = map(int,input("Enter 3 values: ").split())
 
-print(x,y,z)
-print(x+y-z)
+print("The numbers you have entered are : " , x,y,z)
+
+# their sum
+print(f"The sum of {x} , {y} and {z} is : " , x+y+z)
+
+# their difference
+print("Their difference is : ", x-y-z)
+
+# their product
+print("Their product is : " , x*y*z)
+
+# their division 
+print("Their division is : " , x/y/z)
+ 
+# their average 
+print("Their average is : " , (x+y+z)/3)
 
 
 
