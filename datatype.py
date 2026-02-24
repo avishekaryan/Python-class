2e576ff189013340243b4b6f4b3451c02c2a21a6
avishekaryan <ai.avhi.aryan@gmail.com>
2026-02-24 21:05:59 +0545

    Use of int , float , str and bool data types

---
+++ datatype.py
@@ -0,0 +1,24 @@
+# Write a program to show the use of int, float, string, and boolean data types.
+
+# 1. Integer(int) : Used to store whole numbers
+a = 10
+print(f"Value = {a} , Type = {type(a)}")
+
+# 2. Float(float) : Used to store decimal numbers
+b = 8.90976
+print(f"Value = {b} , Type = {type(b)}")
+
+# 3. String(str) : Used to store characters/text
+c = "Avhi"
+print(f"value = {c} , Type = {type(c)}")
+
+# 4. Boolean(bool) : Used to store either True or False
+d = False
+print(f"Value = {d} , Type = {type(d)}")
+
+
+# Use of boolean in a conditional statement
+if d :
+    print("True")
+else :
+    print("false")
