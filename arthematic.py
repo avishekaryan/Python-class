26fcb165b5605ca81f0613e4feda9f2db2a7859b
avishekaryan <ai.avhi.aryan@gmail.com>
2026-02-25 16:12:45 +0545

    Arthematic operators and its use

---
+++ arthematic.py
@@ -0,0 +1,36 @@
+# Write a program to demonstrate arthematic operators (+ - * / % // **)
+
+# Assigning values
+a = 5 
+b = 9
+
+print(f"Initial values : a = {a} and b = {b}")
+
+# Addition operator
+add = a + b 
+print(f"Addition a + b = {a} + {b} = {add}")
+
+# substraction operator
+sub = a - b
+print("Substraction a - b = " , sub)
+
+# Multiplication operator
+mul = a * b 
+print("multiplication  a * b = ", mul)
+
+# Division operator
+div = a / b
+print("Division of a / b = ", div)
+
+# Modulus operator 
+mod = a % b            # gives the remainder of the division
+print("Modulus of a % b = ", mod)
+
+# Floor division operator
+f = a // b            # gives the quotient of the division or divides and return only whole number descarding the decimal part
+print("Floor division of a //b = ", f)
+
+# Exponentiation operator
+e = a ** b            # raises first number to power of second number
+print("Exponentiation of a ** b = ", e)
+
