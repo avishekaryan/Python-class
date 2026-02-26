c2f0628ba7e72e303892feb9d236bf1a63149ec1
avishekaryan <ai.avhi.aryan@gmail.com>
2026-02-26 23:35:16 +0545

    Area and Perimeter of a Circle

---
+++ circle.py
@@ -0,0 +1,13 @@
+# WAP to input radius and calculate area of circle
+# Formula : Area = pi*r^2
+
+
+r = int(input("Enter radius of a circle = "))
+pi = 3.14
+
+area = pi*r*r 
+print("Area of a Circle = " , area)
+
+# Also 
+perimeter = 2*pi*r
+print("Perimeter of a Circle = ", perimeter)
