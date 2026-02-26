fa08e7d4c68f88977a8b363d74794013dfe11427
avishekaryan <ai.avhi.aryan@gmail.com>
2026-02-26 23:18:33 +0545

    Area and Perimeter of rectangle

---
+++ rectangle.py
@@ -0,0 +1,13 @@
+# WAP to input length and breadth and calculate area of rectangle.
+# Formula : Area = length * breadth
+
+l = float(input("Enter given length = "))
+b = float(input("Enter given breadth = "))
+
+print(f"Area of Rectangle = length * breadth = {l} * {b} = " , l*b)
+
+
+
+# Also, Perimeter
+
+print(f"Perimeter of Rectangle = 2(l+b) = 2({l}+{b}) = ", 2*(l+b))
